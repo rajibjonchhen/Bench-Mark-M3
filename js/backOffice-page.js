fbab156e2a38d34ecc30b0a89f50e9d39c61067d
rajibjonchhen <rajib.jonchhen@gmail.com>
2021-12-10 15:46:18 +0000

    fetching

--- js/backOffice-page.js
+++ js/backOffice-page.js
@@ -1,102 +1,199 @@
 
-let movieId = new URLSearchParams(window.location.search)
 
-let url = movieId? "https://striveschool-api.herokuapp.com/api/movies/" + movieId : "https://striveschool-api.herokuapp.com/api/movies" 
-let method = movieId? "PUT":"POST"
-
-    const submit = function(event){
-        event.preventDefault()
-        let form = document.querySelector("form")
-        form.classList.add("validated")
-        const newMovie = {
-            name: document.getElementById("movie-name").value,
-            description: document.getElementById("description").value,
-            category: document.getElementById("category").toLowerCase().value,
-            imageUrl: document.getElementById("imageUrl").value
-        }
-        
-        fetch("https://striveschool-api.herokuapp.com/api/movies", {
-            method:"POST",
-            body:JSON.stringify(newMovie),
-            headers: {
-            'Content-Type': 'application/json',
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYWU1OTRjZmY1ZjAwMTU5MGJkYWYiLCJpYXQiOjE2MzkxMzAwOTksImV4cCI6MTY0MDMzOTY5OX0.s2tZXViAm7UEvRtsxq8dzSl2IfgwSUd33HySA5gYoa4"
-            }
-        })
-        .then(response => response.json())
-        .then(jsonData=>{
-            alert('hello')
-          setTimeout( alertBox("Successfully posted new movie","success"),3000)
-        //   window.location.assign("/")
-        })
+    
+          const movieId = new URLSearchParams(window.location.search).get("movieId")
+          const url = movieId? "https://striveschool-api.herokuapp.com/api/movies/" + movieId: "https://striveschool-api.herokuapp.com/api/movies"
+          const method = movieId? "PUT":"POST"
+          const deleteBtn = document.getElementById("deleteBtn")
          
-        .catch(err=>{
-            alertBox(err,"danger")
-        })
-    }
+          
+         
 
-    // =====
-    fetch("https://striveschool-api.herokuapp.com/api/movies/romantic", {
-            method: "GET", 
+
+          const handleSubmit = function(event){
+            event.preventDefault()
+        
+            const newMovie= {
+                name: document.getElementById("movie-name").value,
+                description: document.getElementById("description").value,
+                category: document.getElementById("category").value.toLowerCase(),
+                imageUrl: document.getElementById("imageUrl").value
+            }
+           
+          
+            fetch("https://striveschool-api.herokuapp.com/api/movies", {
+            method:"POST",
+            body:JSON.stringify(newMovie),    
             headers: {
+                'Content-Type': 'application/json',   
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYWU1OTRjZmY1ZjAwMTU5MGJkYWYiLCJpYXQiOjE2MzkxMzAwOTksImV4cCI6MTY0MDMzOTY5OX0.s2tZXViAm7UEvRtsxq8dzSl2IfgwSUd33HySA5gYoa4"
+            }
+                })
+            .then(response => response.json())
+            .then(movies=>{
+                console.log(movies)    
+                setTimeout( alertBox("Successfully posted new product","success"),3000)
             
+            })
+             
+            .catch(err=>{
+                alertBox(err,"danger")
+            })
+        }
+        
+
+      
+
+
+
+          const handleDelete = async function(){
+            confirm(`Do you want to delete product with id ${movieId}?`)
+            if(confirm){
+              fetch(url, {
+                method:"DELETE",
+            headers: {
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYWU1OTRjZmY1ZjAwMTU5MGJkYWYiLCJpYXQiOjE2MzkxMzAwOTksImV4cCI6MTY0MDMzOTY5OX0.s2tZXViAm7UEvRtsxq8dzSl2IfgwSUd33HySA5gYoa4"
+
+            }
+          })
+          
+            }
+
+          }
+    
+          const alertBox = function(msg,color){
+            let alertBox = document.querySelector("#alertBox")
+            alertBox.innerHTML =""
+            alertBox.classList.add (`bg-${color}`)
+            alertBox.innerHTML =`${msg}`
+            }
+
+
+          
+        
+          
+        
+        window.onload = function(){
+          if(movieId){
+            deleteBtn.style.display = "block"
+            document.querySelector(".h4").innerText = "-Edit Mode-"
+            fetch(url, {
+              method,
+          headers: {
             "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYWU1OTRjZmY1ZjAwMTU5MGJkYWYiLCJpYXQiOjE2MzkxMzAwOTksImV4cCI6MTY0MDMzOTY5OX0.s2tZXViAm7UEvRtsxq8dzSl2IfgwSUd33HySA5gYoa4"
-        }
-    })
-    .then(response => response.json())
-    .then(jsonData=>{
-    console.log(jsonData)
-   
-    })
-     
-    .catch(err=>{
-        alertBox(err,"danger")
-    })
-
-
-    fetch("https://striveschool-api.herokuapp.com/api/movies/horror", {
-        method: "GET", 
-        headers: {
         
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYWU1OTRjZmY1ZjAwMTU5MGJkYWYiLCJpYXQiOjE2MzkxMzAwOTksImV4cCI6MTY0MDMzOTY5OX0.s2tZXViAm7UEvRtsxq8dzSl2IfgwSUd33HySA5gYoa4"
-        }
-    })
-    .then(response => response.json())
-    .then(jsonData=>{
-    console.log(jsonData)
-   
-    })
-     
-    .catch(err=>{
-        alertBox(err,"danger")
-    })
+          }
+      })
+      .then(response => response.json())
+      .then(movies =>{
+        document.getElementById("movie-name").value =movies.name
+        document.getElementById("description").value =movies.description
+        document.getElementById("category").value =movies.category
+        document.getElementById("imageUrl").value =movies.imageUrl
+      }).catch(err => alertBox(err,"danger"))
 
-
-    fetch("https://striveschool-api.herokuapp.com/api/movies/kid", {
-        method: "GET", 
-        headers: {
         
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYWU1OTRjZmY1ZjAwMTU5MGJkYWYiLCJpYXQiOjE2Mzg5Njg5MjIsImV4cCI6MTY0MDE3ODUyMn0.I62rqMBiig_57YNcK0VzC7hRA65djJbbJ_wdeyHseRc"
+          }
         }
-    })
-    .then(response => response.json())
-    .then(jsonData=>{
-    console.log(jsonData)
+
+
+
+
+// ==============================================================
+
+// let movieId = new URLSearchParams(window.location.search)
+
+// let url = movieId? "https://striveschool-api.herokuapp.com/api/movies/" + movieId : "https://striveschool-api.herokuapp.com/api/movies" 
+// let method = movieId? "PUT":"POST"
+
+//     const submitPost = function(event){
+//         event.preventDefault()
+//         // let form = document.querySelector("form")
+//         // form.classList.add("validated")
+//         const newMovie = {
+//             name: document.getElementById("movie-name").value,
+//             description: document.getElementById("description").value,
+//             category: document.getElementById("category").value.toLowerCase(),
+//             imageUrl: document.getElementById("imageUrl").value
+//         }
+        
+//         fetch("https://striveschool-api.herokuapp.com/api/movies", {
+//             method:"POST",
+//             body:JSON.stringify(newMovie),
+//             headers: {
+//             'Content-Type': 'application/json',
+//             "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYWU1OTRjZmY1ZjAwMTU5MGJkYWYiLCJpYXQiOjE2MzkxMzAwOTksImV4cCI6MTY0MDMzOTY5OX0.s2tZXViAm7UEvRtsxq8dzSl2IfgwSUd33HySA5gYoa4"
+//             }
+//         })
+//         .then(response => response.json())
+//         .then(jsonData=>{
+//             alert('hello')
+//           setTimeout( alertBox("Successfully posted new movie","success"),3000)
+//         //   window.location.assign("/")
+//         })
+         
+//         .catch(err=>{
+//             alertBox(err,"danger")
+//         })
+//     }
+
+//     // =====
+//     fetch("https://striveschool-api.herokuapp.com/api/movies/romantic", {
+//             method: "GET", 
+//             headers: {
+            
+//             "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYWU1OTRjZmY1ZjAwMTU5MGJkYWYiLCJpYXQiOjE2MzkxMzAwOTksImV4cCI6MTY0MDMzOTY5OX0.s2tZXViAm7UEvRtsxq8dzSl2IfgwSUd33HySA5gYoa4"
+//         }
+//     })
+//     .then(response => response.json())
+//     .then(jsonData=>{
+//     console.log(jsonData)
    
-    })
+//     })
      
-    .catch(err=>{
-        alertBox(err,"danger")
-    })
-// =====
+//     .catch(err=>{
+//         alertBox(err,"danger")
+//     })
 
-const alertBox = function(msg,color){
-    let alertBox = document.querySelector("#alertBox")
-    alertBox.innerHTML =""
-    alertBox.classList.add (`bg-${color}`)
-    alertBox.innerHTML =`${msg}`
-    }
 
-    window.onload = function(){
+//     fetch("https://striveschool-api.herokuapp.com/api/movies/horror", {
+//         method: "GET", 
+//         headers: {
+        
+//         "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYWU1OTRjZmY1ZjAwMTU5MGJkYWYiLCJpYXQiOjE2MzkxMzAwOTksImV4cCI6MTY0MDMzOTY5OX0.s2tZXViAm7UEvRtsxq8dzSl2IfgwSUd33HySA5gYoa4"
+//         }
+//     })
+//     .then(response => response.json())
+//     .then(jsonData=>{
+//     console.log(jsonData)
+   
+//     })
+     
+//     .catch(err=>{
+//         alertBox(err,"danger")
+//     })
 
-    }
+
+//     fetch("https://striveschool-api.herokuapp.com/api/movies/kid", {
+//         method: "GET", 
+//         headers: {
+        
+//         "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYWU1OTRjZmY1ZjAwMTU5MGJkYWYiLCJpYXQiOjE2Mzg5Njg5MjIsImV4cCI6MTY0MDE3ODUyMn0.I62rqMBiig_57YNcK0VzC7hRA65djJbbJ_wdeyHseRc"
+//         }
+//     })
+//     .then(response => response.json())
+//     .then(jsonData=>{
+//     console.log(jsonData)
+   
+//     })
+     
+//     .catch(err=>{
+//         alertBox(err,"danger")
+//     })
+// // =====
+
+
+
+//     window.onload = function(){
+
+//     }
--- js/script.js
+++ js/script.js
@@ -13,64 +13,34 @@ const hideAppear1 = function(){
 
 
 
-window.onload = async function(){
+
+const loadMovies = function(genreArr){
+
+    for(let i = 0; i < genreArr.length; i++ ){
+        fetch("https://striveschool-api.herokuapp.com/api/movies/" + genreArr[i] , {
+        method:"GET",
+        headers: {   
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYWU1OTRjZmY1ZjAwMTU5MGJkYWYiLCJpYXQiOjE2MzkxMzAwOTksImV4cCI6MTY0MDMzOTY5OX0.s2tZXViAm7UEvRtsxq8dzSl2IfgwSUd33HySA5gYoa4"
+        }
+            })
+        .then(response => response.json())
+        .then(data=>{
+            console.log(data)    
+        })
+         
+        .catch(err=>{
+            alert(err,"danger")
+        })
+    }
+}
+
+window.onload =  function(){
     let genreArr = ["romantic","horror","action"]
 
-
-//         // genreArr.forEach(genre => {
-            fetch("https://striveschool-api.herokuapp.com/api/movies/romantic", {
-            headers: {
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYWU1OTRjZmY1ZjAwMTU5MGJkYWYiLCJpYXQiOjE2MzkxMzAwOTksImV4cCI6MTY0MDMzOTY5OX0.s2tZXViAm7UEvRtsxq8dzSl2IfgwSUd33HySA5gYoa4"
-                    }
-            })
-            .then(response => response.json())
-            .then(jsonData => {
-                console.log(jsonData)
-                let container = document.querySelector(".container")
-                container.innerHTML = ` <section>
-                <div class="row">
-                    <div class="row">
-                        <div class="ml-4 mt-3 d-flex">
-                            <h1 class="pr-3">TV Shows</h1>
-                            <div class="dropdown mt-2">
-                                <button class="btn btn-outline-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-expanded="false" style="border-radius: 2px;">
-                                    Genres
-                                </button>
-                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                    <div class="d-block">
-                                        <a class="dropdown-item" href="#">Action</a>
-                                        <a class="dropdown-item" href="#">Comedy</a>
-                                        <a class="dropdown-item" href="#">Family</a>
-                                        <a class="dropdown-item" href="#">Kids</a>
-                                        <a class="dropdown-item" href="#">Crime</a>
-                                    </div>
-                                </div>
-                                <div class="btn-group me-2" role="group" aria-label="First group" style="border-radius: 2px;">
-                                    <button type="button" class="btn btn-outline-secondary" style="border-radius: 2px;">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-text-left" viewBox="0 0 16 16">
-                                            <path
-                                                fill-rule="evenodd"
-                                                d="M2 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"
-                                            ></path>
-                                        </svg>
-                                    </button>
-                                    <button type="button" class="btn btn-outline-secondary" style="border-radius: 2px;">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-grid" viewBox="0 0 16 16">
-                                            <path
-                                                d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zM2.5 2a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zM1 10.5A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3z"
-                                            ></path>
-                                        </svg>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section> `
-            })
-        .catch(err =>{console.error(err)})
-//  })
-  
+            loadMovies(genreArr)
 
 
+
+              
+               
 }
